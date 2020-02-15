cb77aa1582fce07c7c3236ae70cbafbe010af67f
Abilio Gambim Parada <abiliogp@gmail.com>
2020-02-15 11:17:33 +0000

    [Lint] Changes to deal with Lint

--- routeUITests/RouteUITests.swift
+++ routeUITests/RouteUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class routeUITests: XCTestCase {
+class RouteUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
